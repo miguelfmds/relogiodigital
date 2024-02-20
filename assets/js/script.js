a96941b037ce1f945a3b06d0b32b2402342f1fb9
Miguel Francisco <157380435+miguelfmds@users.noreply.github.com>
2024-02-20 15:30:32 -0300

    Adiciona data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
+const dias = document.getElementById('dia');
+const mes = document.getElementById('mes');
+const ano = document.getElementById('ano');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -16,4 +19,17 @@ const relogio = setInterval(function time() {
     minutos.textContent = min;
     segundos.textContent = sec;
 
-})
+});
+const data = setInterval(function date(){
+    let dateToday = new Date();
+    let day = dateToday.getDate();
+    let month = dateToday.getMonth() + 1;
+    let year = dateToday.getFullYear();
+
+    if(day < 10) day = '0' + day;
+    if(month < 10) month = '0' + month;
+
+    dias.textContent = day;
+    mes.textContent = month;
+    ano.textContent = year;
+});
